b098535b1effad79c6608053880738ed0928b3df
Eric Wang <eric.wang@hurricanesoft.com.tw>
2024-11-12 00:10:23 +0800

    新增自適應動量估計 (Adam) 類別，實作參數更新邏輯；更新 AdaGrad 類別以整合新算法

--- common/update.py
+++ common/update.py
@@ -47,3 +47,30 @@ class AdaGrad:
         for key in params.keys():
             self.h[key] += grads[key] * grads[key]
             params[key] -= self.learning_rate * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+class Adam:
+    """
+    自適應動量估計 (Adam)
+    """
+    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+
+        self.iter += 1
+        lr_t = self.learning_rate * np.sqrt(1.0 - self.beta2 ** self.iter) / (1.0 - self.beta1 ** self.iter)
+
+        for key in params.keys():
+            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+            self.v[key] += (1 - self.beta2) * (grads[key] ** 2 - self.v[key])
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
